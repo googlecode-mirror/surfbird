834eaf903a40cdab473c6ab6147af1ba7d04f765
gxcreator <gxcreator@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-10 18:10:48 +0000

    блог

---
+++ trunk/system/application/views/header.php
@@ -0,0 +1,48 @@
+
+<html>
+    <HEAD>
+	<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<TITLE>SurfBird</TITLE>
+	<META name="description" content="Surfing Bird - программа Попутчик">
+	<META name="keywords" content="SirfBird, Surfing Bird, попутчик, пробки">
+
+<!-- Icons -->
+	<LINK rel="icon" href="images/icons/favicon.ico" type="image/vnd.microsoft.icon">
+	<LINK rel="shortcut icon" href="images/icons/favicon.ico">
+	<LINK rel="apple-touch-icon" href="images/icons/icon_big.png">
+
+<!-- CSS -->
+	<LINK rel="stylesheet" href="<?=base_url()?>css/style.css" type="text/css">
+	<LINK rel="stylesheet" href="<?=base_url()?>css/download.css" type="text/css">
+
+<!-- Javascripts -->
+
+</HEAD>
+<DIV id="header">
+<A href="index.html" id="SBLogo" title="SurfBird.com"><IMG src="./images/SBlogo.png" width="62" height="55" alt="Surfing Bird Logo"></A>
+
+<DIV id="headerNavigation">
+<SPAN id="loggedin-welcome" style="display: none">Привет, <SPAN id="loggedin-username"></SPAN>!</SPAN>
+<SPAN id="loggedin-links" style="display: none"><A href="#">Профиль</A> ·</SPAN>
+<SPAN id="loggedin-settings" style="display: none"><A href="#">Настройки</A> ·</SPAN>
+<SPAN id="loggedin-signin"><A href="#">Войти в SurfBird</A> ·</SPAN>
+<SPAN id="loggedin-signout" style="display: none"><A href="#">Выйти из сети</A> ·</SPAN>
+<A href="help/">Помощь</A> ·
+<FORM id="SearchForm" method="get" action="">
+	<DIV id="searchSet">
+		<INPUT type="search">
+	</DIV>
+</FORM><!-- #SearchForm -->
+</DIV><!-- #headerNavigation -->
+</DIV><!-- #header -->
+<DIV id="mainNavigation">
+<UL>
+	<LI class="active"><A href="#" title="Page One"><STRONG>One</STRONG></A></LI>
+	<LI><A href="#" title="Page Two">Two</A></LI>
+	<LI><A href="#" title="Page Three">Three</A></LI>
+	<LI><A href="#" title="Page Four">Four</A></LI>
+	<LI><A href="#" title="Page Five">Five</A></LI>
+</UL>
+</DIV><!-- #mainNavigation -->
+        </html>
+
