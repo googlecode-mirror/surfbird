c4c31934473f0cf8e7114a573f6c5aff72a0cbcb
gxcreator <gxcreator@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-10 18:16:15 +0000

    блог

--- trunk/system/application/views/welcome_view.php
+++ trunk/system/application/views/welcome_view.php
@@ -1,6 +1,13 @@
 <html>
 <body>
-<h1>      <?echo $content; ?>      </h1>
-</body>
-</html>    
-print:;
+<?
+foreach ($query->    result() as $row)
+{
+        echo "<h1>    ".$row->    title."</h1>    ";
+        echo "<p>    ".$row->    body."</p>    ";
+        echo anchor('welcome/comments/'.$row->    id,'comments');
+}
+
+echo $this->calendar->generate(2006, 6);
+?>
+</body>    
