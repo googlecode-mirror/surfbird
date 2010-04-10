214b973ac47242e9de43340f60a35a5d4c48a614
gxcreator <gxcreator@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-10 18:15:43 +0000

    блог

--- system/application/config/config.php
+++ system/application/config/config.php
@@ -23,7 +23,7 @@ $config['base_url']	= "http://www.surfbird.local/";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "index.php";
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
