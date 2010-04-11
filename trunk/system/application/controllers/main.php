270c94a19f1881533771a2d9a49d1a98ac384937
kuprik17 <kuprik17@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-11 11:54:29 +0000

    Теперь все работает на стилях - у всех страниц один вид.

--- trunk/system/application/controllers/main.php
+++ trunk/system/application/controllers/main.php
@@ -1,14 +1,24 @@
-<?
-Class Main extends Controller
-  {
-    function Main()
-     {
-      parent::Controller();
-     }
-     function index()
-     {
-        $data['content']='Welcome to Surfing Bird!';
-        $this->load->view('main_view',$data);  
-     }
-   }
-?>  
+<?php
+
+class Main extends Controller {
+
+    function __construct()
+    {
+        parent::Controller();    
+    }
+    
+    function index()
+    {
+		// Массив данных
+        $data['title']  = 'Главная';
+		
+        // Подгрузка страниц из одноименных файлов
+		// $partials = array('content'=>'about', 'sidebar'=>'help')
+        $partials = array('content'=>'about', 'sidebar'=>'hello');
+		
+		// Создание и загрузка страницы с данными
+        $this->template->load('template_view', $partials, $data);
+		
+    }
+}
+?>
---
+++ trunk/system/application/views/about.php
@@ -0,0 +1,5 @@
+<h1>Small Template Library</h1>
+
+<p>You just called your first template!</p>
+
+<p><br />Page rendered in {elapsed_time} seconds</p>
