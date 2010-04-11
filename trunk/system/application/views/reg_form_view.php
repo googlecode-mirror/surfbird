56ad0172c4fd0ce10f87e23b4af04f014ce4c1de
kuprik17 <kuprik17@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-11 16:38:38 +0000

    Редактирование CSS, доработка регистрации, создание валидации полей при регистрации. Добавил русский язык ошибок.

--- trunk/system/application/views/reg_form_view.php
+++ trunk/system/application/views/reg_form_view.php
@@ -1,7 +1,66 @@
-﻿  <h1>Регистрация</h1>
-  <?php echo form_open('reg_submit'); ?>
-  <p><label for="login">Логин: </label><br /><?php echo form_input($login); ?></p>
-  <p><label for="email">E-mail: </label><br /><?php echo form_input($email); ?></p>
-  <p><label for="pass">Пароль: </label><br /><?php echo form_input($pass); ?></p>
-  <?php echo form_submit('submit', 'Завершить регистрацию'); ?>
-  <?php echo form_close(); ?>
+﻿<h1>Регистрация<br /><info>Для работы с сервисом необходимо пройти регистрацию</info></h1>
+<?php echo $this->validation->error_string; ?>
+<?php echo form_open('registration'); ?>
+<table>
+	<tr>
+		<td colspan=2><h2>Обязательная информация</h2></td>
+	</tr>
+	<tr>
+		<td width=150>Логин</td>
+		<td><?php echo form_input($login); ?>*</td>
+	</tr>
+	<tr>
+		<td>Пароль</td>
+		<td><?php echo form_input($password); ?>*</td>
+	</tr>
+	<tr>
+		<td>Подтвердите пароль</td>
+		<td><?php echo form_input($passconf); ?>*</td>
+	</tr>
+	<tr>
+		<td>E-mail</td>
+		<td><?php echo form_input($email); ?>*</td>
+	</tr>
+	<tr>
+		<td colspan=2><h2>Контактная информация</h2></td>
+	</tr>
+	<tr>
+		<td>ICQ</td>
+		<td><?php echo form_input($icq); ?></td>
+	</tr>
+	<tr>
+		<td>Моб. телефон</td>
+		<td><?php echo form_input($phone); ?></td>
+	</tr>
+	<tr>
+		<td>Skype</td>
+		<td><?php echo form_input($skype); ?></td>
+	</tr>
+	<tr>
+		<td>ID Вконтакте</td>
+		<td><?php echo form_input($vk); ?></td>
+	</tr>
+	<tr>
+		<td colspan=2><h2>Дополнительная информация</h2></td>
+	</tr>
+	<tr>
+		<td>Имя</td>
+		<td><?php echo form_input($name); ?></td>
+	</tr>
+	<tr>
+		<td>Пол</td>
+		<td><?php echo form_radio($male); ?>Мужской<br />
+			<?php echo form_radio($female); ?>Женский</td>
+	</tr>
+	<tr>
+		<td>Вы водите машину?</td>
+		<td><?php echo form_radio($car); ?>Да<br />
+			<?php echo form_radio($no_car); ?>Нет</td>
+	</tr>
+	<tr>
+		<td colspan=2>
+		<center><?php echo form_submit('submit', 'Завершить регистрацию'); ?></center>
+		</td>
+	</tr>
+</table>
+<?php echo form_close(); ?>
