fb3fbc0ea18182946ecd3fc44ea2fe4db5097892
kuprik17 <kuprik17@1a841ba7-8478-8562-794d-7b4e3c01274c>
2010-04-11 13:12:37 +0000

    Подправил CSS. Начал делать регистрацию.

---
+++ system/application/controllers/registration.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+class Registration extends Controller {
+
+    function __construct()
+    {
+        parent::Controller();    
+    }
+    
+    function index()
+    {
+		
+		#Input and textarea field attributes
+		$data['login'] = array('name' => 'login', 'id' => 'login');
+		$data['email'] = array('name' => 'email', 'id' => 'email');
+		$data['pass'] = array('name' => 'pass', 'id' => 'pass');
+
+		// Массив данных
+        $data['title']  = 'Регистрация';
+		
+        // Подгрузка страниц из одноименных файлов
+		// $partials = array('content'=>'about', 'sidebar'=>'help')
+        $partials = array('content'=>'reg_form_view', 'sidebar'=>'hello');
+		
+		// Создание и загрузка страницы с данными
+        $this->template->load('template_view', $partials, $data);
+		
+    }
+}
+?>
---
+++ system/application/views/reg_form_view.php
@@ -0,0 +1,7 @@
+﻿  <h1>Регистрация</h1>
+  <?php echo form_open('reg_submit'); ?>
+  <p><label for="login">Логин: </label><br /><?php echo form_input($login); ?></p>
+  <p><label for="email">E-mail: </label><br /><?php echo form_input($email); ?></p>
+  <p><label for="pass">Пароль: </label><br /><?php echo form_input($pass); ?></p>
+  <?php echo form_submit('submit', 'Завершить регистрацию'); ?>
+  <?php echo form_close(); ?>
